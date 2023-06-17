f563fac8673b7bed6c98f04e3ae68a3e4b81c91e
Georgi St. Lukanov <goshiii87@gmail.com>
2023-06-17 13:04:15 +0200

    Update settings.py

--- Expenses_App/settings.py
+++ Expenses_App/settings.py
@@ -54,7 +54,8 @@ ROOT_URLCONF = 'Expenses_App.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates']
+        ,
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
