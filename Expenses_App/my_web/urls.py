c7b2231fc9303dcb13c37d326d9d5711cda01b25
Georgi St. Lukanov <goshiii87@gmail.com>
2023-06-17 13:27:10 +0200

    views and urls created and connected

--- Expenses_App/my_web/urls.py
+++ Expenses_App/my_web/urls.py
@@ -1,1 +1,32 @@
-urlpatterns = ()
+from django.urls import path, include
+
+from Expenses_App.my_web.views import index, create_expanse, edit_expanse, delete_expanse, profile_details, \
+    edit_profile, delete_profile
+
+urlpatterns = (
+
+    path('', include([
+        path('', index, name='index'),
+        path('create/', create_expanse, name='create-expanse'),
+        path('edit/<int:pk>/', edit_expanse, name='edit-expanse'),
+        path('delete/<int:pk>/', delete_expanse, name='delete-expanse'),
+    ])),
+
+    path('profile/', include([
+        path('', profile_details, name='profile-details'),
+        path('edit/', edit_profile, name='edit-profile'),
+        path('delete/', delete_profile, name='delete-profile'),
+    ])),
+)
+
+"""
+http://localhost:8000/ - home page
+
+http://localhost:8000/create/ - create expense page
+http://localhost:8000/edit/1/ - edit expense page
+http://localhost:8000/delete/1/ - delete expense page
+
+http://localhost:8000/profile/ - profile page
+http://localhost:8000/profile/edit/ - profile edit page
+http://localhost:8000/profile/delete/ - delete profile page
+"""
--- Expenses_App/my_web/views.py
+++ Expenses_App/my_web/views.py
@@ -1,3 +1,36 @@
 from django.shortcuts import render
 
-# Create your views here.
+from Expenses_App.my_web.models import Profile
+
+
+def index(request):
+    profile = True
+
+    if profile:
+        return render(request, 'common/home-with-profile.html')
+
+    return render(request, 'common/home-no-profile.html')
+
+
+def create_expanse(request):
+    return render(request, 'expense/expense-create.html')
+
+
+def edit_expanse(request, pk):
+    return render(request, 'expense/expense-edit.html')
+
+
+def delete_expanse(request, pk):
+    return render(request, 'expense/expense-delete.html')
+
+
+def profile_details(request):
+    return render(request, 'profile/profile.html')
+
+
+def edit_profile(request):
+    return render(request, 'profile/profile-edit.html')
+
+
+def delete_profile(request):
+    return render(request, 'profile/profile-delete.html')
